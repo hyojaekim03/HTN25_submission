5e3d5bd2783c096fbd991c4193985b44175ca663
hyojaekim03 <hyojaekim03@gmail.com>
2025-02-09 23:45:49 -0500

    push tsconfig.json

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -16,52 +16,52 @@ router.get('/', async (req: Request, res: Response) => {
 
 // Fetch user information by userId
 router.get('/:userId', async (req: Request, res: Response):Promise<any> => {
-    try {
-        const { userId } = req.params;
-    
-        if (!userId) {
-          return res.status(400).json({ message: 'UserId is required' });
-        }
-    
-        const user = await userService.getUserById(parseInt(userId, 10))
-    
-        res.status(200).json(user);
-      } catch (error) {
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-  });
-  
-
-  router.patch('/:userId', async (req: Request, res: Response):Promise<any> => {
-    try {
+  try {
       const { userId } = req.params;
-      let {name, phone_num} = req.body;
-
-      console.log('name and phone: ', name, phone_num)
   
       if (!userId) {
-        return res.status(400).json({ message: 'UserId is required.' });
+        return res.status(400).json({ message: 'UserId is required' });
       }
   
-      if (!name && !phone_num) {
-        return res.status(400).json({ message: 'No fields to update.' });
-      }
-
-      if (name) {
-        name = name.trim(); 
-      }
-
-      const updatedUser = await userService.updateUserById(parseInt(userId, 10), name, phone_num);
+      const user = await userService.getUserById(parseInt(userId, 10))
   
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found.' });
-      }
-  
-      res.status(200).json(updatedUser);
+      res.status(200).json(user);
     } catch (error) {
-      console.error('Error updating user:', error);
       res.status(500).json({ message: 'Internal Server Error' });
     }
-  });
+});
+  
+
+router.patch('/:userId', async (req: Request, res: Response):Promise<any> => {
+  try {
+    const { userId } = req.params;
+    let {phone_num, name} = req.body;
+
+    console.log('name and phone: ', name, phone_num)
+
+    if (!userId) {
+      return res.status(400).json({ message: 'UserId is required.' });
+    }
+
+    if (!name && !phone_num) {
+      return res.status(400).json({ message: 'No fields to update.' });
+    }
+
+    if (name) {
+      name = name.trim(); 
+    }
+
+    const updatedUser = await userService.updateUserById(parseInt(userId, 10), name, phone_num);
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 export default router;
